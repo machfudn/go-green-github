581537c0d9094992d0b0b9fac4797f451d99a078
Machfudin <machfudin37@gmail.com>
2025-06-19 11:15:10 +0700

    mengubah commit per hari menjadi 50

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import simpleGit from "simple-git";
 
 const dataPath = "./data.json";
 const git = simpleGit();
-const commitsPerDay = 30; // Jumlah commit per hari
+const commitsPerDay = 50; // Jumlah commit per hari
 
 const today = moment().tz("Asia/Jakarta");
 
